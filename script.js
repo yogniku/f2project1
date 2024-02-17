27902439f63af0477abe52ebadf53518f692691c
Yogesh3712 <113026686+Yogesh3712@users.noreply.github.com>
2024-02-17 16:07:46 +0530

    add

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 /** @format */
-
-let arr = [
+ let students = [
   { id: 1, name: "john", age: "18", marks: 80 },
   { id: 2, name: "jack", age: "20", marks: 85 },
   { id: 3, name: "karen", age: "19", marks: 35 },
@@ -8,20 +7,60 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(arr => {
+    if (arr.marks > 50) {
+      console.log(`Name: ${arr.name}, Marks: ${arr.marks}`);
+    }
+  });
+}
+function PrintStudentbyForEach() {
+  students.forEach(student => {
+    if (student.marks > 50) {
+      console.log(`${student.name} scored ${student.marks} marks.`);
+    }
+  });
 }
 
-function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
-}
+PrintStudentbyForEach();
 
 function addData() {
-  //Write your code here, just console.log
+  let newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
+  students.push(newStudent);
+  console.log("New student added:");
+  console.log(newStudent);
 }
+
+addData();
+console.log("Updated students array:");
+console.log(students);
+
 
 function removeFailedStudent() {
+  for (let i = students.length - 1; i >= 0; i--) {
+    if (students[i].marks < 50) {
+      students.splice(i, 1);
+    }
+  }
+  console.log("Removed students who failed:");
+  console.log(students);
   //Write your code here, just console.log
 }
+removeFailedStudent();
+function ConcatenateArray() {
+  let additionalStudents = [
+    { id: 4, name: "emma", age: "21", marks: 90 },
+    { id: 5, name: "alex", age: "19", marks: 75 },
+    { id: 6, name: "olivia", age: "22", marks: 60 }
+  ];
 
-function concatenateArray() {
-  //Write your code here, just console.log
+  const concatenatedArray = [...students, ...additionalStudents];
+
+  console.log("Concatenated array:");
+  console.log(concatenatedArray);
 }
+ConcatenateArray();
+
+
+
+  //Write your code here, just console.log
+
